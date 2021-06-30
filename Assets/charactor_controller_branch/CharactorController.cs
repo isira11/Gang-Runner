154de82ab1fe78ba31ad29ee3ec72b732e7415b2
Isira <88isira88@gmail.com>
2021-06-29 22:21:06 -0400

    added delta movement

--- Assets/charactor_controller_branch/CharactorController.cs
+++ Assets/charactor_controller_branch/CharactorController.cs
@@ -4,44 +4,45 @@ using UnityEngine;
 
 public class CharactorController : MonoBehaviour
 {
-    
-    public Vector2 mouse_pos = Vector2.zero;
-    public float z_offset = 0;
 
-    float y_screen_point_offset;
+    Vector3 intersect_0;
+    Vector3 pos_0;
 
 
     void Update()
     {
-        y_screen_point_offset = Camera.main.WorldToScreenPoint(new Vector3(0,0, z_offset)).y;
 
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            Ray cam_ray = Camera.main.ScreenPointToRay(new Vector2(Input.mousePosition.x, y_screen_point_offset));
+            intersect_0 = GetIntersectPoint();
+            pos_0 = transform.position;
+        }
 
-
-            Vector3 line1_point = cam_ray.origin;
-            Vector3 line2_point = new Vector3(line1_point.x, 0, line1_point.z);
-
-            Vector3 line1_dir = cam_ray.direction;
-            Vector3 line2_dir = new Vector3(cam_ray.direction.x, 0, cam_ray.direction.z);
-
-            Debug.DrawRay(line1_point, line1_dir * 100, Color.black);
-            Debug.DrawRay(line2_point, line2_dir * 100, Color.red);
-
-			Vector3 intersection;
-
-			if (Math3d.LineLineIntersection(out intersection, line1_point, line1_dir, line2_point, line2_dir))
-            {
-                transform.position = intersection;
-                print(intersection);
-                ///print(intersection);
-
-			}
-
-
-
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 delta = GetIntersectPoint() - intersect_0;
+            transform.position = pos_0 + delta;
         }
     }
+
+    public Vector3 GetIntersectPoint()
+    {
+        float y_screen_point_offset = Camera.main.WorldToScreenPoint(transform.position).y;
+        Ray cam_ray = Camera.main.ScreenPointToRay(new Vector2(Input.mousePosition.x, y_screen_point_offset));
+
+        Vector3 line1_point = cam_ray.origin;
+        Vector3 line2_point = new Vector3(line1_point.x, 0, line1_point.z);
+
+        Vector3 line1_dir = cam_ray.direction;
+        Vector3 line2_dir = new Vector3(cam_ray.direction.x, 0, cam_ray.direction.z);
+
+        Debug.DrawRay(line1_point, line1_dir * 100, Color.black);
+        Debug.DrawRay(line2_point, line2_dir * 100, Color.red);
+        Math3d.LineLineIntersection(out Vector3 intersection, line1_point, line1_dir, line2_point, line2_dir);
+
+        return intersection;
+
+    }
+
 
 }
